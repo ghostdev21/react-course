b317ddac54643fcb11788a626edbb216acdf6f52
Ghostdev <daniel21develop@gmail.com>
2026-01-03 16:27:57 -0600

    add eslint

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,14 @@ import { checkWinner } from "./utils/board"
 function App() {
 
 
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.x)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.x
+  })
   const [winner, setWinner] = useState(null)
 
 
@@ -19,6 +25,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.x)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
 
@@ -32,6 +41,9 @@ function App() {
 
     const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
     setTurn(newTurn)
+
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
 
     const newWinner = checkWinner(newBoard)
     if(newWinner) {
